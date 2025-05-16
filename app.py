1184bc96a7b6c58fce279df86d09f8bab9751103
fearfully-m <kmstevensj@gmail.com>
2025-05-16 01:05:29 -0400

    Add Search, Search JS effect, start Advanced Search

--- app.py
+++ app.py
@@ -4,16 +4,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def search():
-    return render_template("search.html")
+    return render_template("search.html",
+    heading = 'Google Clone', 
+    title ='Google Clone',
+    show_title = True)
 
 @app.route("/advanced")
 def advanced_search():
-    return render_template("advancedsearch.html")
+    return render_template("advancedsearch.html",
+    heading = 'Advanced Search',
+    title = 'Advanced Search',
+    show_title = True)
 
 
 @app.route("/image")
 def image_search():
-    return render_template("imagesearch.html")
+    return render_template("imagesearch.html",
+    heading = 'Image Search',
+    title = 'Image Search',
+    show_title = True)
 
 if __name__ == "__main__":
     app.run(debug=True)
