b7c3dba3c5090667956571f44a7c6af5364a1083
Fearfully-M <kmstevensj@gmail.com>
2025-05-16 12:46:18 -0400

    update venv

--- Project0/bin/activate.fish
+++ Project0/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/fearfully_m/GoogleClone/Project0
+set -gx VIRTUAL_ENV /home/fearfully_m/Desktop/GoogleClone/Project0
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
